f0e97f668d40e9e18e072ce3b068b5f4c975d5b6
snigdhauppula <73580523+snigdhauppula@users.noreply.github.com>
2020-10-30 13:14:57 +0530

    Add files via upload

---
+++ BikeMethodOverriding.java
@@ -0,0 +1,36 @@
+abstract class Bike{
+abstract void run();
+abstrat void speed();
+void changeGear(){
+System.out.println("gear changed");
+}
+}
+class Honda extends Bike{
+void run(){
+System.out.println("Honda is running safely");
+}
+void speed(){
+System.out.println("Honda speed is 60");
+}
+}
+class Pulsar extends Bike{
+void cost(){
+System.out.println("Pulsar is running safely");
+}
+void speed(){
+System.out.println("pulsar speed is 80");
+}
+}
+class BikeMethodOverriding{
+public static void main(String args[]){
+Bike x=new Honda();
+x.changeGear();
+x.run();
+x.speed();
+x=new pulsar();
+x.changedGear();
+x.run();
+x.speed();
+}
+}
+
