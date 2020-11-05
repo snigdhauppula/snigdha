527b19622bb89689a34b45865d811d8e58ff1d5d
snigdhauppula <73580523+snigdhauppula@users.noreply.github.com>
2020-11-05 16:52:19 +0530

    Update StringBufferDemo.java

--- StringBufferDemo.java
+++ StringBufferDemo.java
@@ -6,7 +6,7 @@ System.out.println(sb);
 System.out.println(sb.charAt(1));
 System.out.println(sb.append("uppula"));
 System.out.println(sb.equals(s));
-System.out.println(sb.indexOf("h"));
+System.out.println(sb.indexOf("m"));
 System.out.println(sb.substring(2));
 System.out.println(sb.substring(1,4));
 System.out.println(sb.reverse());
