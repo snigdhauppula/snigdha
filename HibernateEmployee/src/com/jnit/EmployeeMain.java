797e84000e2d45268840f55e4e741d5482df5e23
snigdhauppula <73580523+snigdhauppula@users.noreply.github.com>
2020-12-18 21:35:15 +0530

    this is my fourth commit

--- HibernateEmployee/src/com/jnit/EmployeeMain.java
+++ HibernateEmployee/src/com/jnit/EmployeeMain.java
@@ -10,15 +10,11 @@ import org.hibernate.cfg.Configuration;
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		Configuration cf=new Configuration();
-		cf.configure("configuration.xml");
-		SessionFactory sf=cf.buildSessionFactory();
-		Session se=sf.openSession();
+		Session se=Config.config();
 		Transaction tx=se.beginTransaction();
-		Employee emp=new Employee();
 		tx.commit();
 		Scanner sc=new Scanner(System.in);
-		System.out.println("1.Insert \n2.Display \n3.Update \n4.Delete \n5.Exit");
+		System.out.println("1.Insert\n 2.Display\n 3.Update\n 4.Delete\n 5.Exit");
 		int number=0;
 		for(;number<=5;) {
 			System.out.println("Enter a number between 1 and 5");
