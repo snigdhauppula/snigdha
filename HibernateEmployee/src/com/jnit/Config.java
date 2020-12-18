7c24f4933ca8836e02d5e5d1d0abf7cb083c9f69
snigdhauppula <73580523+snigdhauppula@users.noreply.github.com>
2020-12-18 21:22:41 +0530

    this is my fourth commit

---
+++ HibernateEmployee/src/com/jnit/Config.java
@@ -0,0 +1,15 @@
+package com.jnit;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Config {
+		public static Session config() {
+			Configuration cf=new Configuration();
+			cf.configure("configuration.xml");
+			SessionFactory sf=cf.buildSessionFactory();
+			Session se=sf.openSession();
+			return se;
+		}
+}
