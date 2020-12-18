4b9d2425b035e98912793302cb9fdb0ad4315018
snigdhauppula <73580523+snigdhauppula@users.noreply.github.com>
2020-12-18 21:30:03 +0530

    this is my fourth commit

--- HibernateEmployee/src/com/jnit/Display.java
+++ HibernateEmployee/src/com/jnit/Display.java
@@ -1,25 +1,17 @@
 package com.jnit;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
-
 
 public class Display {
 	public static void display() {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter 1.display all records 2.display particular record");
 		int choice=sc.nextInt();
-		Configuration cf=new Configuration();
-		cf.configure("configuration.xml");
-		SessionFactory sf=cf.buildSessionFactory();
-		Session se=sf.openSession();
+		Session se=Config.config();
 		Transaction tx=se.beginTransaction();
 		if(choice==1) {
 			Query qr=se.createQuery("from Employee emp");
@@ -42,5 +34,4 @@ public class Display {
 			}
 		}
 	}
-
 }
