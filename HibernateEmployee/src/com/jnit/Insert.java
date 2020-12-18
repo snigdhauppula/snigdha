3bff4b3b7a85549ce9e5e92f2ca23c2a05315025
snigdhauppula <73580523+snigdhauppula@users.noreply.github.com>
2020-12-18 21:37:31 +0530

    this is my fourth commit

--- HibernateEmployee/src/com/jnit/Insert.java
+++ HibernateEmployee/src/com/jnit/Insert.java
@@ -10,13 +10,10 @@ import org.hibernate.cfg.Configuration;
 public class Insert {
 	public static void insert() {
 		Scanner sc=new Scanner(System.in);
+		Session se=Config.config();
 		String choice;
 		do {
 			System.out.println("Enter employee details(eid,ename,esal,department,experience)");
-			Configuration cf=new Configuration();
-			cf.configure("configuration.xml");
-			SessionFactory sf=cf.buildSessionFactory();
-			Session se=sf.openSession();
 			Employee emp=new Employee();
 			Transaction tx=se.beginTransaction();
 			emp.setId(sc.nextInt());
@@ -24,7 +21,9 @@ public class Insert {
 			emp.setSal(sc.nextInt());
 			emp.setDepartment(sc.next());
 			emp.setExperience(sc.nextInt());
-			se.save(emp);
+			Object s=se.save(emp);
+			if(s!=null)
+				System.out.println("Record inserted");
 			tx.commit();
 			System.out.println("Do you want to continue (y/n)");
 			choice=sc.next();
